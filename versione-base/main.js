a85586d3fbc343d2b6801c66f02a2afb9598cce2
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-15 15:19:21 +0200

    MILESTONE 2

--- versione-base/main.js
+++ versione-base/main.js
@@ -3,37 +3,57 @@ const image =['01.webp','02.webp','03.webp','04.webp','05.webp'];
 // console.log(image.map(element=> element+30));
 // AGGIUNGO IL RESTO DELL'LINK A OGNI ELEMENTO DELL'ARRAY
 const imgLink = image.map(element => `http://127.0.0.1:5500/img/${element}` );
-console.log (imgLink);
+
 
 let index = 0;
-const prev = document.querySelector('.prev');
-prev.addEventListener ('click' ,function(){
-    index++
-    imgsrc.src = imgLink[index];
-});
+console.log(imgLink.length)
+const imglenght = imgLink.length -1;
+console.log (imglenght);
+
 
 const next = document.querySelector('.next');
 next.addEventListener ('click' ,function(){
-    index--
+    if (index === imglenght){
+        index = 0;
+    }else{
+
+        index++
+    }
+    
     imgsrc.src = imgLink[index];
+    console.log(image[index]);
+    
+});
+
+
+
+
+const prev = document.querySelector('.prev');
+prev.addEventListener ('click' ,function(){
+    if (index === 0){
+        index = 4;
+    }else{
+
+        index--
+    }
+    
+    imgsrc.src = imgLink[index];
+    console.log(image[index]);
+    
+   
+    
 });
 
 
 
 
 
-if (prev){
-    index--
-    console.log (index);
-}else if (next){
-    index++
-    console.log (index);
-}
-console.log (index);
+
+
 
 
 
 
 
 const imgsrc = document.querySelector('img');
-console.log (imgsrc.src)
+
